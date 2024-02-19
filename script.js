eb7382f0d724f5680372ea5c71de566d97cb339c
Oleg Kireev <kuru@mail.ru>
2024-02-19 23:11:51 +0300

    TASK-9 Store state

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 var checkbox = document.querySelector('.theme-switch__checkbox');
+var theme = localStorage.getItem('data-theme');
+
+if (theme != null && theme == 'dark') {
+  const btnTheme = document.getElementById('btnTheme');
+  btnTheme.checked = true;
+  transition();
+  document.documentElement.setAttribute('data-theme', 'dark');
+}
 
 checkbox.addEventListener('change', function () {
   transition();
   if (this.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('data-theme', 'dark');
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('data-theme', 'light');
   }
 })
 
